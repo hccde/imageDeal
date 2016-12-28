2cf833545a4ea48f7c859698b62ef0548e5dc89e
hccde <1076663958@qq.com>
2016-12-28 23:48:59 +0800

    换promise 来处理图片加载的异步

---
+++ class/index.js
@@ -0,0 +1,54 @@
+class Image {
+	constructor(url){
+		console.log(url);
+	function _getImageData(url){
+		let handle = load_img(url);
+		function* load_img(src){
+			let img = new Image();
+			console.log(src);
+			img.src = src;
+				let canvasEle = document.createElement('CANVAS');
+				let ctx = canvasEle.getContext('2d');
+				let imageData;
+				img.onload = function(){
+					let {height,width} = img;
+					canvasEle.height = height;
+					canvasEle.width = width;
+					ctx.drawImage(img, 0, 0);
+					imageData= ctx.getImageData(0, 0, width, height);
+					ctx.clearRect(0,0,imageData.width,imageData.height);
+					console.log('over')
+					handle.next();
+			}
+			yield imageData;
+			return imageData;
+			console.log('返回')
+		}
+	}
+		_getImageData(url);
+	}
+	static _getImageData(url){
+		let handle = load_img(url);
+		function* load_img(src){
+			let img = new Image();
+			img.src = src;
+				let canvasEle = document.createElement('CANVAS');
+				let ctx = canvasEle.getContext('2d');
+				let imageData;
+				img.onload = function(){
+					let {height,width} = img;
+					canvasEle.height = height;
+					canvasEle.width = width;
+					ctx.drawImage(img, 0, 0);
+					imageData= ctx.getImageData(0, 0, width, height);
+					ctx.clearRect(0,0,imageData.width,imageData.height);
+					console.log('over')
+					handle.next();
+			}
+			yield imageData;
+			return imageData;
+			console.log('返回')
+		}
+	}
+}
+module.exports = Image;
